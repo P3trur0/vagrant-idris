60a539030f86eb688da46947e92e3e0b6e2e8ac9
Peter Pan <P3trur0@users.noreply.github.com>
2018-07-20 15:18:06 +0200

    content formatting

--- idris.sh
+++ idris.sh
@@ -4,23 +4,23 @@
 source /home/vagrant/utils.sh
 
 liquid_prompt() {
-    if [ ! -f ~/runonce ] #here the check is on the home directory
-    then
-		msg ':::: setting up liquidprompt to vagrant machine ::::'
-		msg "Installing liquidprompt..."
-		cd ~
-		git clone https://github.com/nojhan/liquidprompt.git
+  if [ ! -f ~/runonce ] #here the check is on the home directory
+  then
+    msg ':::: setting up liquidprompt to vagrant machine ::::'
+    msg "Installing liquidprompt..."
+    cd ~
+    git clone https://github.com/nojhan/liquidprompt.git
     echo "Setting up liquidprompt to the .bashrc folder"
-		echo '# Only load Liquid Prompt in interactive shells, not from a script or from scp'  >> ~/.bashrc
-		echo '[[ $- = *i* ]] && source ~/liquidprompt/liquidprompt' >> ~/.bashrc
-		touch ~/runonce	#avoid script to be provisioned more than once
-	else
-		msg "::: liquidprompt already provisioned :::"
-	fi
+    echo '# Only load Liquid Prompt in interactive shells, not from a script or from scp'  >> ~/.bashrc
+    echo '[[ $- = *i* ]] && source ~/liquidprompt/liquidprompt' >> ~/.bashrc
+    touch ~/runonce	#avoid script to be provisioned more than once
+  else
+    msg "::: liquidprompt already provisioned :::"
+  fi
 }
 
 idris() {
-	sudo add-apt-repository ppa:hvr/ghc --yes
+  sudo add-apt-repository ppa:hvr/ghc --yes
   sudo apt-get update
   sudo apt-get install ghc-7.10.3 cabal-install-2.0 --yes
   msg "GHC and Cabal installed"
@@ -29,10 +29,10 @@ idris() {
 
   msg "Setting environment variables after installation..."
 
-	msg "Installing idris ..."
+  msg "Installing idris ..."
   cabal update
-	cabal install idris
-	msg "Idris installed!"
+  cabal install idris
+  msg "Idris installed!"
 
   echo 'export PATH=/opt/ghc/7.10.3/bin/:/opt/cabal/2.0/bin/:$HOME/.cabal/bin/:$PATH' >> /home/vagrant/.bashrc
   source /home/vagrant/.bashrc
@@ -42,7 +42,7 @@ idris() {
 
 
 main() {
-	idris
+  idris
   liquid_prompt
   exit 0
 }
